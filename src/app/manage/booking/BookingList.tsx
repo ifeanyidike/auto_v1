2ed6aaf6f5a1002554aac79d2471dffb948f5aee
Ifeanyi Dike <lorddickson751@gmail.com>
2024-01-20 15:17:45 +0100

    feat - improve booking page

--- src/app/manage/booking/BookingList.tsx
+++ src/app/manage/booking/BookingList.tsx
@@ -30,7 +30,7 @@ const BookingList = () => {
         ref={popupRef}
         className={`bg-white absolute right-5 w-48 z-50 text-content-normal text-xs flex-flex-col items-center rounded-xl border border-stone-200`}
         style={{
-          top: popupOpen ? popupOpen - 75 : 0,
+          top: popupOpen ? popupOpen - 120 : 0,
         }}
       >
         <button className="flex gap-2 w-full items-center p-4 hover:bg-stone-200 hover:rounded-t-xl">
