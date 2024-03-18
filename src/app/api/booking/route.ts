0c1b75e81d7299da31cd01df6d6d7d066b156f62
Ifeanyi Dike <lorddickson751@gmail.com>
2024-03-18 23:55:33 +0100

    remove some routes

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -9,25 +9,25 @@ type CreateParams = {
   userId: string;
 };
 
-export const POST = async (req: Request) => {
-  const { serviceId, code, interval, userId } =
-    (await req.json()) as CreateParams;
+// export const POST = async (req: Request) => {
+//   const { serviceId, code, interval, userId } =
+//     (await req.json()) as CreateParams;
 
-  const subscriptionPlan = new SubscriptionPlan();
-  const plan = await subscriptionPlan.findItem({ code, interval });
-  const planId = plan?.id;
-  if (!planId) return;
+//   const subscriptionPlan = new SubscriptionPlan();
+//   const plan = await subscriptionPlan.findItem({ code, interval });
+//   const planId = plan?.id;
+//   if (!planId) return;
 
-  const subscription = new Subscription();
-  const newSubscription = await subscription.create(serviceId, planId, userId);
-  return new Response(JSON.stringify(newSubscription));
-};
+//   const subscription = new Subscription();
+//   const newSubscription = await subscription.create(serviceId, planId, userId);
+//   return new Response(JSON.stringify(newSubscription));
+// };
 
-export const GET = async (req: NextRequest) => {
-  const url = new URL(req.url);
-  const limit = url.searchParams.get('limit');
-  const subscription = new Subscription();
-  const subscriptionList = await subscription.getMany(limit);
-  console.log('subscriptionList', subscriptionList);
-  return new Response(JSON.stringify(subscriptionList));
-};
+// export const GET = async (req: NextRequest) => {
+//   const url = new URL(req.url);
+//   const limit = url.searchParams.get('limit');
+//   const subscription = new Subscription();
+//   const subscriptionList = await subscription.getMany(limit);
+//   console.log('subscriptionList', subscriptionList);
+//   return new Response(JSON.stringify(subscriptionList));
+// };
